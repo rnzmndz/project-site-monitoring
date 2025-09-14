0f0e3d94d3f005e24b543f339a6ca9ece918fd0f
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-14 19:11:59 +0800

    add auditing

--- account-service/src/main/java/site/renzoproject/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/site/renzoproject/account_service/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package site.renzoproject.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
