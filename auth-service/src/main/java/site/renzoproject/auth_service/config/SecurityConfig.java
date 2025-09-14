04b678cc214451bd4ac2b420e621b8685514b56b
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-14 16:28:42 +0800

    update auth-service config

--- auth-service/src/main/java/site/renzoproject/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/site/renzoproject/auth_service/config/SecurityConfig.java
@@ -16,10 +16,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
                 );
+
         return http.build();
     }
 
