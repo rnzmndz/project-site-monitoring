443f2a62087e389c54acc6d22a3f7df1819372c1
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-21 18:27:52 +0800

    udpate application

--- backend-service/application/src/main/java/site/renzoproject/Application.java
+++ backend-service/application/src/main/java/site/renzoproject/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "site.renzoproject")
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @RestController
 public class Application {
