0fe929520f4f2f46ec566ad2a70debc088788520
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-21 17:26:58 +0800

    update backend service and add employee module

---
+++ backend-service/employee-service/src/main/java/site/renzoproject/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package site.renzoproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    public String Employee(){
+        return "This is employee controller";
+    }
+}
