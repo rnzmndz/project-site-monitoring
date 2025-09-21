df5d68caf41ec376b1748cb8d25f6dc8a01851b3
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-21 21:11:01 +0800

    update

--- backend-service/application/src/main/java/site/renzoproject/Application.java
+++ backend-service/application/src/main/java/site/renzoproject/Application.java
@@ -2,11 +2,13 @@ package site.renzoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.renzoproject.config.EmployeeModuleConfig;
 
 @SpringBootApplication
+@Import(EmployeeModuleConfig.class)
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @RestController
 public class Application {
