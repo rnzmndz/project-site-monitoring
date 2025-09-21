3dd1602335a2ccc3be7f9d63e609858ca04b7667
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-21 08:06:59 +0800

    update Discovery Service

---
+++ discovery-service/src/main/java/site/renzoproject/discovery_service/config/EurekaServerConfig.java
@@ -0,0 +1,6 @@
+package site.renzoproject.discovery_service.config;
+
+// Uncomment this on production
+//@Configuration
+public class EurekaServerConfig {
+}
---
+++ discovery-service/src/main/java/site/renzoproject/discovery_service/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package site.renzoproject.discovery_service.config;
+
+// Enable this on production
+//@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/actuator/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic();
+//        return http.build();
+//    }
+}
