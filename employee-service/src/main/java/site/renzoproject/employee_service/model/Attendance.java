2fd989a709a1ae3dceebeeb3eeff24f2915481fe
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-09-23 21:31:32 +0800

    update the models

--- employee-service/src/main/java/site/renzoproject/employee_service/model/Attendance.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/model/Attendance.java
@@ -2,9 +2,14 @@ package site.renzoproject.employee_service.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -35,4 +40,17 @@ public class Attendance {
     private String status; // PRESENT, LATE, ABSENT, HALF_DAY
 
     private String source; // BIOMETRIC, MANUAL, SYSTEM
+
+    //Auditing fields
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
 }
--- employee-service/src/main/java/site/renzoproject/employee_service/model/Leave.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/model/Leave.java
@@ -34,4 +34,5 @@ public class Leave {
 
     @Column(columnDefinition = "text")
     private String reason;
+
 }
