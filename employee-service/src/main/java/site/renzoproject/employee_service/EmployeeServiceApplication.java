b3e30d8cad39bf97fea66129fa6bbd04d862324b
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-10-03 07:14:54 +0800

    enable OpenFeign

--- employee-service/src/main/java/site/renzoproject/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/EmployeeServiceApplication.java
@@ -2,8 +2,10 @@ package site.renzoproject.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "site.renzoproject.employee_service.client")
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
