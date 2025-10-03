aeda633034a87979595cf35f53eb13203f1d2c27
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-10-03 20:00:47 +0800

    add errorresponsedto

---
+++ employee-service/src/main/java/site/renzoproject/employee_service/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package site.renzoproject.employee_service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private Map<String, String> details;
+    private String path;
+}
