e8b65888383c7203688b1c4a59423d47a6f881d3
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-10-03 19:59:06 +0800

    update Employee Repository

--- employee-service/src/main/java/site/renzoproject/employee_service/repository/EmployeeRepository.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/repository/EmployeeRepository.java
@@ -3,7 +3,17 @@ package site.renzoproject.employee_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.renzoproject.employee_service.model.Employee;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    Optional<Employee> findByAccountId(UUID accountId);
+
+    List<Employee> findByDepartmentContainingIgnoreCase(String department);
+
+    List<Employee> findByJobTitleContainingIgnoreCase(String jobTitle);
+
+    List<Employee> findByHiredDateAfter(LocalDate date);
 }
