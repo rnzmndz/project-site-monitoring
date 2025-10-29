fe22c747b1e122e2133f46bd9f8d0eeb9a4e6882
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-10-29 14:38:09 +0800

    add security requirement in OpenAPI config

--- employee-service/src/main/java/site/renzoproject/employee_service/config/OpenApiConfig.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/config/OpenApiConfig.java
@@ -1,6 +1,8 @@
 package site.renzoproject.employee_service.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -16,6 +18,9 @@ import java.util.List;
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
+)
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 public class OpenApiConfig {
 
