b771c6b71ffffa2459cc07129c1204553ce8c023
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-10-29 15:22:01 +0800

    add AuditorAware in annotation

--- employee-service/src/main/java/site/renzoproject/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/site/renzoproject/employee_service/EmployeeServiceApplication.java
@@ -3,9 +3,11 @@ package site.renzoproject.employee_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "site.renzoproject.employee_service.client")
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
