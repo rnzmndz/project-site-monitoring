def67339d55954d8f6874178cc018b560baaf1d9
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-11-14 15:10:53 +0800

    uncomment getUsername in AuthController

--- auth-service/src/main/java/site/renzoproject/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/site/renzoproject/auth_service/controller/AuthController.java
@@ -214,14 +214,14 @@ public class AuthController {
         return ResponseEntity.ok(Map.of("authenticated", isAuthenticated));
     }
 
-//    @GetMapping("/{userId}/username")
-//    public ResponseEntity<String> getUsername(@PathVariable String userId) {
-//        try {
-//            String username = keycloakService.getUsername(userId);
-//            return ResponseEntity.ok(username);
-//        } catch (Exception e) {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
+    @GetMapping("/{userId}/username")
+    public ResponseEntity<String> getUsername(@PathVariable String userId) {
+        try {
+            String username = keycloakService.getUsername(userId);
+            return ResponseEntity.ok(username);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
