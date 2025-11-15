abf06c66a69c282b5cee504e068ae10768f2bd79
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-11-15 21:59:24 +0800

    allow preflight request

--- auth-service/src/main/java/site/renzoproject/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/site/renzoproject/auth_service/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package site.renzoproject.auth_service.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow prefligh
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
