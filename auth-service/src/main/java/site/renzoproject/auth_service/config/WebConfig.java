271f00145a380c736c9c102781f450ac2e14d6d6
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-11-22 18:24:52 +0800

    add Etag on auth-service

---
+++ auth-service/src/main/java/site/renzoproject/auth_service/config/WebConfig.java
@@ -0,0 +1,19 @@
+package site.renzoproject.auth_service.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class WebConfig {
+
+    //This will help to lessen the call of the API and fasten the load of the data
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilterFilterRegistrationBean() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new ShallowEtagHeaderFilter());
+        filter.addUrlPatterns("/*");
+        return filter;
+    }
+}
